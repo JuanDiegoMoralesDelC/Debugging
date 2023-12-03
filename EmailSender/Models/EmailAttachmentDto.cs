7af721eedcfb5914c94822faa6c8ffdb57d14c02
jdmdelc@gmail.com <jdmdelc@gmail.com>
2023-12-03 00:25:48 -0500

    Added Email models and appsettings variables

---
+++ EmailSender/Models/EmailAttachmentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EmailSender.Models
+{
+    public class EmailAttachmentDto
+    {
+        public bool HasAttachment { get; set; }
+        public MemoryStream File { get; set; }
+        public string FileName { get; set; }
+        public string MediaType { get; set; }
+    }
+}
---
+++ EmailSender/Models/EmailDto.cs
@@ -0,0 +1,13 @@
+﻿namespace EmailSender.Models
+{
+    public class EmailDto
+    {
+        public string From { get; set; }
+        public string FromName { get; set; }
+        public string To { get; set; }
+        public string ToName { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsHtml { get; set; }
+    }
+}
